f697e9ea24d1809d9430c5bf66633f37ddc1298e
AdrianK98 <adrian.k.koczur@gmail.com>
2023-04-02 10:00:18 +0200

    changed sw.js

--- sw.js
+++ sw.js
@@ -5,12 +5,6 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll([
         'index.html',
-        'map.html',
-        'sos.html',
-        'https://cdnjs.cloudflare.com/ajax/libs/openlayers/4.6.5/ol.js',
-        'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css',
-        'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-        'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js',
       ]);
     })
   );
